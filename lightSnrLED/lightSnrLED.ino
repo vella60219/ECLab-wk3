701f4535661e3cab766c47aaad1675431354aba6
vella60219 <83958906+vella60219@users.noreply.github.com>
2021-10-05 21:41:35 +0800

    Add files via upload

---
+++ lightSnrLED/lightSnrLED.ino
@@ -0,0 +1,14 @@
+void setup() {
+  Serial.begin(115200); //設定傳輸鮑率，跟電腦傳輸資料的頻率
+  pinMode(12, INPUT); //設定腳位為輸入用
+
+  ledcSetup(0, 5000, 8);   //頻率5000，頻道0，解析度8;
+  ledcAttachPin(14, 0);  //將上述頻道0附加到ledPin腳位14上
+}
+
+void loop() {
+  int input = analogRead(12);
+  int input_map = map(input, 0, 4095, 0, 255);
+  Serial.println(input);
+  ledcWrite(0, input_map);
+}
